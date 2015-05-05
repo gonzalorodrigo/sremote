8a750920f29efe0d4c3f108e933469058a930625
Gonzalo Rodrigo <gonzalo@cs.umu.se>
2015-05-05 16:51:37 -0700

    qdo_interpreter_newt.sh module command absoulte route

--- qdo_interpreter_newt.sh
+++ qdo_interpreter_newt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-module load python
-module load virtualenv
+/opt/modules/3.2.10.2/bin/modulecmd tcsh load python
+/opt/modules/3.2.10.2/bin/modulecmd tcsh load virtualenv
 cd ~/qdo_interpreter
 source env/bin/activate
 python qdo_remote_server.py "${1}"
