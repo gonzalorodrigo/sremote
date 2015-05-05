3c03ff3f8828d6a832c130a61d8a513715da8518
Gonzalo Rodrigo <gonzalo@cs.umu.se>
2015-05-05 11:12:26 -0700

    Improved function names and code commenting

--- qdo_remote_api.py
+++ qdo_remote_api.py
@@ -1,3 +1,25 @@
+""" 
+Tool-box to do remote Python functions calls.
+
+The class RemoteCliente and RemoteServer use these methods to serialize and
+deserialize the method call request and the method response.
+
+The method call request is a dictionary with two items:
+    - dic[COMMAND_TYPE]: string with  of the method to be executed
+    - dic[COMMAND_ARGS]: list of arguments of the method to be executed.
+e.g. call for methods
+    foo(arg1, arg2)
+    {COMMAND_TYPE: "foo", COMMAND_ARGS: [val_arg1, val_arg2]}
+
+The method response is a dictionary with two times:
+    - dic[RESPONSE_STATUS]: bool, True if the remote method call got to end.
+    - dic[RESPONSE_CONTENT]: if status == True then the return value of the
+      remote call. If status == False then a string explaining what failed. 
+ 
+ This methods uses xml_marshaller as a serializer, it can be changed by unsing
+ set_serializer
+
+"""
 from xml_marshaller import xml_marshaller
 
 COMMAND_TYPE = "command"
@@ -5,43 +27,82 @@ COMMAND_ARGS = "args"
 RESPONSE_STATUS = "success"
 RESPONSE_CONTENT = "return_value"
 
-def call_method_object_command(obj, command_obj):
-    print command_obj
-    command_name = command_obj[COMMAND_TYPE]
-    args_obj = command_obj[COMMAND_ARGS]
+_serializer = xml_marshaller
+
+def call_method_object_command(obj, call_request):
+    """Executes method in obj as instructed  by call_request.
+    
+    Args:
+        obj: object whose method will be executed.
+        call_request: method call request. It indicates the name of the method
+        to be called and the argument values to be pased.
+    
+    Returns:
+        whatever the called method in obj returns.    
+    """
+    command_name = call_request[COMMAND_TYPE]
+    args_obj = call_request[COMMAND_ARGS]
     return call_method_object(command_name, args_obj)
 
-def call_method_object(obj, name, args):
-    method = getattr(obj, name)
+def call_method_object(obj, method_name, args):
+    """Executes obj.method_name(*args) and returns what ever it returns."""
+    method = getattr(obj, method_name)
     output = method(*args)
     return output
 
-def process_remote_call(remote_obj, command_string):
-    command, args = decode_commnad(command_string)
-    call_method_object(remote_obj, command, args)
+def process_remote_call(invoked_object, request_string):
+    """Deserializes a call_request and executes it in invoked_object.
+    
+    Args:
+        invoked_object: object whose method will be executed.
+        request_string: call_request in serialized format.
+    Returns: 
+        result of executing whatever is specified in request_sting in
+        invoked_object.
+    """ 
+    method_name, args = decode_call_request(request_string)
+    return call_method_object(invoked_object, method_name, args)
 
-def encode_command(command, args = []):
-    command_obj = {COMMAND_TYPE: command}
+def encode_call_request(command_name, args = []):
+    """Creates a call_request_object and serializes it."""
+    command_obj = {COMMAND_TYPE: command_name}
     command_obj[COMMAND_ARGS] = args
     return serialize_obj(command_obj)
     
-def decode_commnad(command_string):
-    obj = deserialize_obj(command_string)
+def decode_call_request(call_request_serialized):
+    """Deserializes and decodes a call_request_object."""
+    obj = deserialize_obj(call_request_serialized)
     return obj[COMMAND_TYPE], obj[COMMAND_ARGS]
     
-def encode_response(obj, success=True):
+def encode_call_response(return_value, success=True):
+    """Encodes and serializes a method response."""
     response = {RESPONSE_STATUS:success}
-    response[RESPONSE_CONTENT] = obj
+    response[RESPONSE_CONTENT] = return_value
     return serialize_obj(response)
 
-def decode_response(response_string):
-    response_obj = deserialize_obj(response_string)
+def decode_call_response(call_response_serialized):
+    """Deserializes and decodes a method response."""
+    response_obj = deserialize_obj(call_response_serialized)
     return response_obj[RESPONSE_STATUS], response_obj[RESPONSE_CONTENT]
  
- 
+def set_serializer(serializer):
+    """Sets the serializer of the library.
+    
+    Args:
+        serializer: an object that implements the methods:
+            - dumps(obj): returns the serialized vesio of obj
+            - loads(serialized_obj): returns the object serialized in
+              seriazlied_obj. 
+    """
+    _serializer = serializer
+
 def serialize_obj(obj):
-    return xml_marshaller.dumps(obj)
+    """return serialized version of obj."""
+    return _serializer.dumps(obj)
    
 def deserialize_obj(command_string):
-    return xml_marshaller.loads(command_string)
+    """returns object serialized in command_string."""
+    return _serializer.loads(command_string)
+
+
 
--- qdo_remote_server.py
+++ qdo_remote_server.py
@@ -6,9 +6,9 @@ if (sys.argv):
     file_route = sys.argv[1]
     text_file = open(file_route, "r")
     content = "\n".join(text_file.readlines())
-    command_name, args = remote.decode_commnad(content)
+    command_name, args = remote.decode_call_request(content)
     #print "COMMMMAND", command_name
     reponse_obj = remote.call_method_object(qdo, command_name, args)
-    print remote.encode_response(reponse_obj, True)
+    print remote.encode_call_response(reponse_obj, True)
 else:
-    print remote.encode_response({}, False)
+    print remote.encode_call_response({}, False)
