2e2c9103e033410318ac3e5851aa9899b416211b
Gonzalo Rodrigo <gonzalo@cs.umu.se>
2015-05-05 18:17:45 -0700

    pass

--- test_qdo_remote_newt.py
+++ test_qdo_remote_newt.py
@@ -1,10 +1,10 @@
 import qdo_remote_api_sim
 import qdo_remote_api_newt
 
-connector = qdo_remote_api_newt.QDONewtConnector("carver")
+connector = qdo_remote_api_newt.QDONewtConnector("hopper")
 client = qdo_remote_api_sim.QDORemoteClient(
             connector)
-if not connector.auth("gprodri", "S4n!amart42015"):
+if not connector.auth("gprodri", ""):
     print "Error auth!"
     exit()
 
