4bb85d6f4eddedbfa6c90186aa51768810c29989
Gonzalo Rodrigo <gonzalo@cs.umu.se>
2015-05-05 17:39:53 -0700

    setup script adds qdo special lib

--- setup_newt.sh
+++ setup_newt.sh
@@ -4,3 +4,7 @@ module load virtualenv
 virtualenv env
 source env/bin/activate.csh
 pip install -r requirements.txt
+
+git clone https://gonzalorodrigo@bitbucket.org/gonzalorodrigo/qdo_remote_exp.git
+cd qdo_remote_exp/py
+python setup.py install
