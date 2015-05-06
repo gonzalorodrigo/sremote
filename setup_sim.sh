478bfffa22154fe6ef7012fbec26ec3a31d7a6ff
Gonzalo Rodrigo <gonzalo@cs.umu.se>
2015-05-05 20:34:20 -0700

    Improved comments. Added setup script for the simulated local end point

---
+++ setup_sim.sh
@@ -0,0 +1,10 @@
+#!/bin/csh
+#This script may not work on Edison, Hopper, or Carver.
+virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+
+git clone https://gonzalorodrigo@bitbucket.org/gonzalorodrigo/qdo_remote_exp.git
+cd qdo_remote_exp/py
+python setup.py install
+mkdir ~\.qdo
