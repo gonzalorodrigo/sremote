a76a84857974a76f0fd2be0aa4815f7f2051faa3
Gonzalo Rodrigo <gonzalo.rodrigo@gmail.com>
2015-09-22 16:08:19 -0700

    SREMOTE_BACKUP_ENV points to the environment folder, not the py file.

--- py/sremote/res/interpreter.sh
+++ py/sremote/res/interpreter.sh
@@ -33,7 +33,7 @@ cd $install_dir
 
 if ( -f "virtualenvs/py2.7/bin/activate.csh" ) then
 	source virtualenvs/py2.7/bin/activate.csh
-	setenv SREMOTE_BACKUP_ENV "$install_dir/virtualenvs/py2.7/bin/activate_this.py" 
+	setenv SREMOTE_BACKUP_ENV "$install_dir/virtualenvs/py2.7/" 
 else
 	if (-f /etc/profile.d/modules.csh) then
 		source /etc/profile.d/modules.csh
@@ -42,7 +42,7 @@ else
 		module load virtualenv
 	endif 
 	source env/bin/activate.csh
-	setenv SREMOTE_BACKUP_ENV "$install_dir/env/bin/activate_this.py" 
+	setenv SREMOTE_BACKUP_ENV "$install_dir/env/" 
 endif
 
 
